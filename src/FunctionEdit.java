bbb04df6d462022d4266cb83bd6ebb82743b3a9c
Solanki Singha <solankisingha23@gmail.com>
2024-02-05 13:57:33 +0530

    FunctionEdit.java is added

---
+++ src/FunctionEdit.java
@@ -0,0 +1,16 @@
+public class FunctionEdit {
+    
+    Gui gui;
+
+    public FunctionEdit(Gui gui){
+        this.gui = gui;
+    }
+
+    public void undo(){
+        gui.um.undo();
+    }
+
+    public void redo(){
+        gui.um.redo();
+    }
+}
